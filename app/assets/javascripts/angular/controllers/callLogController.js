c1995205f796d47fdf1c6eb47fb6be2499efaf9d
kwoodard22 <kwoodard22@gmail.com>
2014-12-15 16:59:33 -0500

    callLogController and dashboard are displaying seeded data

--- app/assets/javascripts/angular/controllers/callLogController.js
+++ app/assets/javascripts/angular/controllers/callLogController.js
@@ -1,9 +1,29 @@
 (function(){
-  Dashboard.controller('callLogController', ['$scope', '$resource', function($scope, $resource) {
+  Dashboard.controller('callLogControllerTest', ['$scope', '$resource', function($scope, $resource) {
       var Calls = $resource('/api/call');
       $scope.calls = Calls.query();
     }]);
+  
+
+  Dashboard.controller('CallLogControllerTest', function() {
+      this.calls = allCalls;
+    });
+
+    var allCalls = [{
+      category: 'Love',
+      phoneNumber: '1234567890'
+    },
+    {
+      category: 'Peace',
+      phoneNumber: '0101010101'
+    }];
+
 })();
+
+
+// TO NOTE:
+// filter for date --> {{'1388123412323' | date:'MM/dd/yyyy @ h:mma'}} 
+// produces --> 12/27/2013 @ 12:50am
 
 // (function(){
 //   Dashboard.controller('callLogController', function() {
